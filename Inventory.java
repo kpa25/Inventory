fcc00e0040490accb1a9e823dc1f3732d512303a
kpa25 <kpa25@scarletmail.rutgers.edu>
2014-11-17 20:32:40 -0500

    Update Inventory.java

--- Inventory.java
+++ Inventory.java
@@ -2,6 +2,7 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.Scanner;
+import java.util.StringTokenizer;
 
 
 public class Inventory {
@@ -24,9 +25,9 @@ public class Inventory {
 	//method that reads the file and stores the variables for the  inventory
 	public void parseFile() throws FileNotFoundException{
 		//all the types of possible inputs in the inventory and their data types
-		String ID;
-		String description;
-		int numStock;
+		String ID = null;
+		String description = null;
+		int numStock = 0;
 		double price;
 		int numSubAssembly;
 		String subAssemblyID;
@@ -35,8 +36,39 @@ public class Inventory {
 		ArrayList<String> subAssemblyNames = new ArrayList<String>();
 		
 		Scanner input= new Scanner(this.inventoryFile);
+		//looking for the spaces in the inventory file and printing it 
 		while(input.hasNextLine()){
-			System.out.println(input.nextLine());
+			String line = input.nextLine();
+			
+			//ignoring the | and printing each item on a new line from the file 
+			StringTokenizer t = new StringTokenizer(line, "|");
+			while(t.hasMoreTokens()){
+				String token = t.nextToken().trim();
+				int counter = 1;
+				switch(counter){
+					case 1:
+						ID= token;
+						break;
+					case 2:
+						description=token;
+						break;
+						//WORK ON CASE 3
+					case 3:
+						break;
+					case 4:
+						numStock= Integer.parseInt(token);
+						break;
+					case 5:
+						price=Double.parseDouble(token);
+					counter=1;
+					Item currItem = new Item( ID, description, qtySubAssembly, subAssemblyNames,numStock, price);
+					kahiniStore.add(currItem);
+						
+						
+				}
+				System.out.println(token);
+				
+			}
 		}
 	}	
 }//end of Inventory class
