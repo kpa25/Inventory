35b32105c1874895eaccab6ebfc20a96105347d7
kpa25 <kpa25@scarletmail.rutgers.edu>
2014-11-24 19:05:32 -0500

    added 2 methods to parse sub assemblies

--- Inventory.java
+++ Inventory.java
@@ -28,7 +28,7 @@ public class Inventory {
 		String ID = null;
 		String description = null;
 		int numStock = 0;
-		double price;
+		double price = 0.0;
 		int numSubAssembly;
 		String subAssemblyID;
 		//array list for the qty for sub assembly  and names of the sub assemblies
@@ -42,33 +42,91 @@ public class Inventory {
 			
 			//ignoring the | and printing each item on a new line from the file 
 			StringTokenizer t = new StringTokenizer(line, "|");
+			//using string tokenizer to turn each element within the | into a token
+			int counter = 1;
 			while(t.hasMoreTokens()){
 				String token = t.nextToken().trim();
-				int counter = 1;
 				switch(counter){
+				//case for the ID associated with the item 
 					case 1:
 						ID= token;
+						counter++;
 						break;
+				//description associated with the item
 					case 2:
 						description=token;
+						counter++;
 						break;
 						//WORK ON CASE 3
+				//Special case; could be missing in a file
 					case 3:
+						if (token.equals("")){
+							System.out.println("Case 3 token is a space");
+						}else{
+							qtySubAssembly = parseSubQty(token);
+							subAssemblyNames = parseSubID(token);
+						}
+						counter++;
 						break;
+				//Special case; could be missing in the file
 					case 4:
-						numStock= Integer.parseInt(token);
+						if (token.equals("")){
+							counter=1;
+							numStock=-1;
+							price=-1;
+							//adding a new item 
+							Item currItem = new Item( ID, description, qtySubAssembly, subAssemblyNames,numStock, price);
+							kahiniStore.add(currItem);
+						}else{
+							counter++;
+							numStock= Integer.parseInt(token);
+						}
 						break;
+				//Special Case; Could be missing in the file
 					case 5:
 						price=Double.parseDouble(token);
-					counter=1;
-					Item currItem = new Item( ID, description, qtySubAssembly, subAssemblyNames,numStock, price);
-					kahiniStore.add(currItem);
-						
-						
-				}
-				System.out.println(token);
+						counter=1;
+						Item currItem = new Item( ID, description, qtySubAssembly, subAssemblyNames,numStock, price);
+						kahiniStore.add(currItem);
+						break;	
+				}//end of switch statement 
+			
+				//System.out.println(token);
 				
-			}
-		}
-	}	
+			}//end of inner while loop 
+		}//end of outer while loop
+	}//end of parse file 
+	
+	//getting the qty from the list of sub assembly 
+	public ArrayList<Integer> parseSubQty(String token){
+		StringTokenizer t= new StringTokenizer(token, ",x");
+		int counter=1; 
+		ArrayList<Integer> temp = new ArrayList<Integer>();
+		
+		while(t.hasMoreTokens()){
+		String tok= t.nextToken().trim();
+			switch(counter){
+				case 1:
+					temp.add(Integer.parseInt(tok));
+					counter++;
+					break;
+				case 2:
+					counter=1; 
+					break;
+			}//end of switch
+		}//end of while
+	
+		/*for(int i =0; i < temp.size(); i++){
+			System.out.println(temp.get(i));
+		}*/
+		return temp;
+		
+	}
+	//getting the ID from the list of sub assemblies
+	//make an arraylist of strings, that represents easch id of the sub assemblies (makes switch, add into the temp array list (string) , print out M, S1...
+	public ArrayList<String> parseSubID(String token){
+		
+		return null;
+		
+	}
 }//end of Inventory class
